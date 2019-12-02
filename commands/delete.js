336aeec910fb6f149b9d4a7c9b6282263e3db42f
Zebby_ <48347420+TwilightZebby@users.noreply.github.com>
2019-12-02 11:22:17 +0000

    Don't talk to me about the Delete CMD p.2

--- commands/delete.js
+++ commands/delete.js
@@ -26,12 +26,10 @@ module.exports = {
 
         if(roleSearch.length > 0) {
           // Turn those Role IDs into Role Mentions!
-          let tempNum = tempNumber + 1;
-          tempNumber++;
-          let roleString = tempNum + ': <@&' + roleSearch[0].userRole + '>';
+          let roleString = tempNumber + ': <@&' + roleSearch[0].userRole + '>';
           data.push(roleString);
         }
-
+        tempNumber++;
       }
 
       // Check how many Roles were found
@@ -52,76 +50,118 @@ module.exports = {
 
       const numRegex = new RegExp(`[0-9]`);
       const filter = m => m.content.match(numRegex);
-      var pOneDone = false; // To help move onto PART TWO
       var userChoice = null; // For fetching that User Choice!
+      var userSecondChoice = null;
 
-      const collectorOne = await message.channel.createMessageCollector(filter, { time: 10000, idle: 10000 })
+      const collectorOne = message.channel.createMessageCollector(filter, { idle: 10000 })
        .on('collect', m => collectorOne.stop())
-       .on('end', (collected, reason) => {
+       .on('end', (collected, reason) => WhenCollectorOneEnds(collected, reason));
+
+
+      async function WhenCollectorOneEnds(collected, reason) {
 
         if(reason == 'time' || reason == 'idle') {
           delEmbed.addField(`⌛ Timeout!`, `Sorry, need to be quicker! 😜`);
           return message.channel.send(delEmbed);
         }
-        
-        pOneDone = true;
+
         const collect = collected.array();
         userChoice = parseInt(collect[0].content);
-      });
 
-      // Should we go to PART TWO?
-      if(pOneDone == false) {
-        return;
-      }
-
-      if(userChoice == null) {
-        delEmbed.addField(`Oops!`, `Something broke. Don't worry, just try again!`);
-        return message.channel.send(delEmbed);
-      } else if(userChoice > roleStoreArray.length) {
-        delEmbed.addField(`Whoops!`, `That number wasn't accepted. Please try again.`);
-        return message.channel.send(delEmbed);
-      }
-
-
-      // ********************************
-      // SECOND PART:
-      //   Use the selected userRole and bring up a list of all the pingRoles attached.
-      //   Then, allow the User to choose which entry to delete.
-      // ********************************
-
-      const selectedUserRole = await RoleData.findAll({ where: { guildID: message.guild.id, userRole: roleStoreArray[userChoice - 1].id }, order: ['pingRole', 'ASC'] })
-      .catch(err => console.error(`ERROR: Something happened. - delete.js selectedUserRole - \n${err}`));
-
-      if(selectedUserRole.length < 1) {
-        delEmbed.addField(`🦆 Quack!`, `Hmmm, this shouldn't be an error!\nIf you get it, ping a message to \<\@156482326887530498\>`);
-        return message.channel.send(delEmbed);
-      }
-
-      var dataTwo = [];
-      var anotherTempNumber = 0;
-      for(let s = 0; s < selectedUserRole.length; s++) {
-
-        let tempNumb = anotherTempNumber + 1;
-        anotherTempNumber++;
-
-        let uPermSymbol = null;
-        let uPerm = null;
-        if(selectedUserRole[s].userPermission == 'deny') {
-          uPermSymbol = "❌";
-          uPerm = "**cannot** mention";
-        } else if(selectedUserRole[s].userPermission == 'allow') {
-          uPermSymbol = "✅";
-          uPerm = "**can** mention";
+        if(userChoice == null) {
+          delEmbed.addField(`Oops!`, `Something broke. Don't worry, just try again!`);
+          return message.channel.send(delEmbed);
+        } else if(userChoice > roleStoreArray.length) {
+          delEmbed.addField(`Whoops!`, `That number wasn't accepted. Please try again.`);
+          return message.channel.send(delEmbed);
+        }
+  
+  
+        // ********************************
+        // SECOND PART:
+        //   Use the selected userRole and bring up a list of all the pingRoles attached.
+        //   Then, allow the User to choose which entry to delete.
+        // ********************************
+        const selectedUserRole = await RoleData.findAll({ where: { guildID: message.guild.id, userRole: roleStoreArray[userChoice].id }, order: [ ['pingRole', 'ASC'] ] })
+        .catch(err => console.error(`ERROR: Something happened. - delete.js selectedUserRole - \n${err}`));
+  
+        if(selectedUserRole.length < 1) {
+          delEmbed.addField(`🦆 Quack!`, `Did you input the correct number? Please try again!`);
+          return message.channel.send(delEmbed);
         }
 
-        let dataString = tempNumb + ': ' + uPermSymbol + ' \<\@\&' + selectedUserRole[s].userRole + '> ' + uPerm + ' \<\@\&' + selectedUserRole[s].pingRole + '\>';
-        dataTwo.push(dataString);
+        const selUserRole = roleStoreArray[userChoice].id;
+        const selUserRoleLength = selectedUserRole.length;
+  
+        var dataTwo = [];
+        var anotherTempNumber = 0;
+        for(let s = 0; s < selectedUserRole.length; s++) {
+  
+          let uPermSymbol = null;
+          let uPerm = null;
+          if(selectedUserRole[s].userPermission == 'deny') {
+            uPermSymbol = "❌";
+            uPerm = "**cannot** mention";
+          } else if(selectedUserRole[s].userPermission == 'allow') {
+            uPermSymbol = "✅";
+            uPerm = "**can** mention";
+          }
+  
+          let dataString = anotherTempNumber + ': ' + uPermSymbol + ' \<\@\&' + selectedUserRole[s].userRole + '> ' + uPerm + ' \<\@\&' + selectedUserRole[s].pingRole + '\>';
+          dataTwo.push(dataString);
+          anotherTempNumber++;
+  
+        }
+  
+        delEmbed.addField(`\u200B`, dataTwo.join(' \n '));
+        delEmbed.addField(`Part Two:`, `Please select which entry you would like to delete by using the number listed`);
+        message.channel.send(delEmbed);
+
+        const collectorTwo = message.channel.createMessageCollector(filter, { idle: 10000 })
+       .on('collect', m => collectorTwo.stop())
+       .on('end', (collected, reason) => WhenCollectorTwoEnds(collected, reason, selUserRole, selUserRoleLength, selectedUserRole));
 
       }
 
-      delEmbed.addField(`\u200B`, dataTwo.join(' \n '));
-      delEmbed.addField(`Part Two:`, `Please select which entry you would like to delete by using the numbers listed`);
-      message.channel.send(delEmbed);
+      // ********************************
+      // PART THREE
+      //    Grab the selected entry, and PURGE IT FROM EXISTANCE MWHAHAHAHAHAH
+      // ********************************
+
+      async function WhenCollectorTwoEnds(collected, reason, selUserRole, selUserRoleLength, selectedUserRole) {
+        delEmbed.spliceField(0, 2);
+
+        if(reason == 'time' || reason == 'idle') {
+          delEmbed.addField(`⌛ Timeout!`, `Sorry, need to be quicker! 😜`);
+          return message.channel.send(delEmbed);
+        }
+
+        const collec = collected.array();
+        userSecondChoice = parseInt(collec[0].content);
+
+        if(userChoice == null) {
+          delEmbed.addField(`Oops!`, `Something broke. Don't worry, just try again!`);
+          return message.channel.send(delEmbed);
+        } else if(userSecondChoice > selUserRoleLength) {
+          delEmbed.addField(`Whoops!`, `That number wasn't accepted. Please try again.`);
+          return message.channel.send(delEmbed);
+        }
+
+        const selectedPingRole = selectedUserRole[userSecondChoice].pingRole;
+
+        const deleteSelectedEntry = await RoleData.destroy({ where: { guildID: message.guild.id, userRole: selUserRole, pingRole: selectedPingRole }, limit: 1 })
+          .catch(err => console.error(`ERROR: Something happened. - delete.js deleteSelectedEntry - \n${err}`));  
+        if(!deleteSelectedEntry) {
+          delEmbed.addField(`🤔`, `Hmmm, I was unable to delete this entry...\nPlease ping a message to \<\@156482326887530498\> if this continues.`);
+          return message.channel.send(delEmbed);
+        }
+
+        delEmbed.addField(`👍`, `Successfully deleted entry.`);
+        return message.channel.send(delEmbed);
+
+      }
+
+      
 
 
       //END OF COMMAND
